2e119d19a4b4ee42f4b66c2376c0646c2c8a2c2d
Farzan <farzanroxz123@gmail.com>
2024-12-18 10:27:26 +0530

    Image Generation
    Implemented and completed image generation api by connecting runware.ai.

--- backend/app/routes/image_generation.py
+++ backend/app/routes/image_generation.py
@@ -9,22 +9,37 @@ from app.models import ImageInput
 router = APIRouter(prefix='/image_generation', tags=["Images"])
 settings = Settings()
 
+system_prompt = """
+Take the input image and creatively transform its design elements while maintaining the original size, shape, structure, view and key architectural or physical features.
+Modify only the stylistic aspects such as colors, textures, patterns, materials, and decor elements according to the specified design style.
+Ensure the output looks visually cohesive, realistic, and aligns with the given style while preserving the core identity and layout of the original image.
+"""
+
 # @router.post('/create_images', response_model=Union[List[IImage], None], status_code=201)
 @router.post('/create_images', status_code=201)
 async def generate_images(data: ImageInput):
-    # return [data.input_image]
+    
     runware = Runware(
         api_key=settings.RUNWARE_API_KEY
     )
     await runware.connect()
-    request_image = IImageInference(
-        positivePrompt="a beautiful sunset over the mountains",
-        model="civitai:36520@76907",
-        numberResults=2,
-        negativePrompt="cloudy, rainy",
-        height=512,
-        width=512,
-        outputType=IOutputType["base64Data"]
-    )
-    images = await runware.imageInference(requestImage=request_image)
-    return images
+
+    results = []
+
+    
+    for styles in data.styles:
+        for style in styles:
+            request_image = IImageInference(
+                positivePrompt=system_prompt + '\n' + data.prompt + '\n' + f'The image should be in {style} style.',
+                model="civitai:36520@76907",
+                numberResults=1,
+                negativePrompt="cloudy, rainy",
+                height=512,
+                width=512,
+                outputType="base64Data",
+                seedImage=data.input_image,
+            )
+            images = await runware.imageInference(requestImage=request_image)
+            results.extend([image.imageBase64Data for image in images])
+
+    return results
